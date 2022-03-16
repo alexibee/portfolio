23955ce2518c72275e95dbf05ca6065dc94531c5
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-03-16 23:47:04 +0000

    reload

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import React from 'react'
 import About from './components/about/About'
-import Projects from './components/projects/Projects'
+import Portfolio from './components/portfolio/Portfolio'
 
 const App = () => {
   return (
     <>
         <About />
-        <Projects/>
+        <Portfolio/>
     </>
   )
 }
