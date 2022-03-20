db5d7d8106dacdc0873a6ca7076aba04bb85cf81
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-03-20 23:51:40 +0000

    css

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -12,8 +12,9 @@ const Header = () => {
         <h5 className="text-light">Full-Stack Developer</h5>
         <CTA />
         <div className="me">
-          <img src={ME} alt="" />
+          <img src={ME} alt="me" />
         </div>
+        <a href="#contact" className='scroll__down'>Scroll Down</a>
       </div>
     </header>
   )
