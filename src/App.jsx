6a71fba1172573c011825d4fdc36faa52d9b75f1
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-03-20 22:45:08 +0000

    ctas and photo

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
+import Header from './components/header/Header'
 import About from './components/about/About'
+import Experience from './components/experience/Experience'
 import Portfolio from './components/portfolio/Portfolio'
+import Footer from './components/footer/Footer'
 
 const App = () => {
   return (
     <>
+        <Header />
         <About />
+        {/* <Experience /> */}
         <Portfolio/>
+        <Footer />
     </>
   )
 }
---
+++ src/components/header/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './header.scss'
+import CTA from './CTA'
+import ME from '../../assets/me.jpg'
+
+const Header = () => {
+  return (
+    <header>
+      <div className='container header__container'>
+        <h5> Hey, I am </h5>
+        <h1>Alexi Brehovs</h1>
+        <h5 className="text-light">Full-Stack Developer</h5>
+        <CTA />
+        <div className="me">
+          <img src={ME} alt="" />
+        </div>
+      </div>
+    </header>
+  )
+}
+
+export default Header
