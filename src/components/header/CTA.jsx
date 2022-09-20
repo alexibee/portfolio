4185bdb6561a05a8965b1785b65670b55ffd3477
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-09-20 10:34:36 +0100

    upd

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const CTA = () => {
   return (
     <div className='cta'>
-        <a href='https://res.cloudinary.com/dhoecmw9w/image/upload/v1663313878/production/Aleksejs_Brehovs_Resume_16-09_gueubx.pdf' download rel='noreferrer' target='_blank' className='btn'>Download CV</a>
+        <a href='https://res.cloudinary.com/dhoecmw9w/image/upload/v1663666397/production/Aleksejs_Brehovs_Resume_20-09-2022_wdqzlw.pdf' download rel='noreferrer' target='_blank' className='btn'>Download CV</a>
         <a href="#contact" className='btn btn-primary'>Contact me</a>
       </div>
   )
