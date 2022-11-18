7bd8ccb2f93a2fe0786d1e7d5d96d3cd29c2bb28
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-11-18 10:45:33 +0000

    proj

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -1,49 +1,58 @@
-import React from "react"
-import './about.scss'
-import {FaAward} from 'react-icons/fa'
-import {VscFolderLibrary} from 'react-icons/vsc'
-import Socials from './Socials'
+import React from 'react';
+import './about.scss';
+import { FaAward } from 'react-icons/fa';
+import { VscFolderLibrary } from 'react-icons/vsc';
+import Socials from './Socials';
 
 const About = () => {
+	return (
+		<section id='about'>
+			<h5>Get To Know</h5>
+			<h2>About Me</h2>
 
-  return (
-    <section id='about'>
-      <h5>Get To Know</h5>
-      <h2>About Me</h2>
+			<div className='container about__container'>
+				<div className='about__me'>
+					<div className='about__me-image'>
+						<img
+							src='https://res.cloudinary.com/dhoecmw9w/image/upload/v1650377128/production/me_ysy0ww.jpg'
+							alt='About'
+						/>
+					</div>
+					<Socials />
+				</div>
 
-      <div className="container about__container">
-        <div className="about__me">
-          <div className="about__me-image">
-            <img src='https://res.cloudinary.com/dhoecmw9w/image/upload/v1650377128/production/me_ysy0ww.jpg' alt="About" />
-          </div>
-          <Socials />
-        </div>
+				<div className='about__content'>
+					<div className='about__cards'>
+						<article className='about__card'>
+							<FaAward className='about__icon' />
+							<h5>Education</h5>
+							<p>2022 - Le Wagon London bootcamp graduate</p>
+							<p>2014 - Music Technology BSc 1st Class Hons</p>
+						</article>
 
-        <div className="about__content">
-          <div className="about__cards">
-            <article className='about__card'>
-              <FaAward className='about__icon'/>
-              <h5>Education</h5>
-              <p>2022 - Le Wagon London bootcamp graduate</p>
-              <p>2014 - Music Technology BSc 1st Class Hons</p>
-            </article>
+						{/* <article className='about__card'>
+							<VscFolderLibrary className='about__icon' />
+							<h5>Projects</h5>
+							<p>3 Projects and counting :) </p>
+						</article> */}
+					</div>
+					<p>
+						Junior Full Stack Developer with professional experience working for
+						a busy fintech start-up, participating in challenging projects both
+						as part of the team and independently, delivering value under tight
+						deadlines.
+					</p>
 
-            <article className='about__card'>
-              <VscFolderLibrary className='about__icon'/>
-              <h5>Projects</h5>
-              <p>2 Projects and counting :) </p>
-            </article>
-          </div>
-          <p>
-          Junior Full Stack Developer with professional experience working for a busy fintech start-up, participating in challenging
-          projects both as part of the team and independently, delivering value under tight deadlines.
-          </p>
-
-          <a href="#contact" className='btn btn-primary'>Let's Talk</a>
-        </div>
-      </div>
-    </section>
-  )
-}
+					<a
+						href='#contact'
+						className='btn btn-primary'
+					>
+						Let's Talk
+					</a>
+				</div>
+			</div>
+		</section>
+	);
+};
 
 export default About;
