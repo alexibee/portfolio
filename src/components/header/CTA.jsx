1d834d3c40c5e90900098f2047b2a98c3c2a286c
Aleksejs Brehovs <aleksi.b.tr@gmail.com>
2022-11-18 12:47:16 +0000

    cv upd

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
+import React from 'react';
 
 const CTA = () => {
-  return (
-    <div className='cta'>
-        <a href='https://res.cloudinary.com/dhoecmw9w/image/upload/v1663666397/production/Aleksejs_Brehovs_Resume_20-09-2022_wdqzlw.pdf' download rel='noreferrer' target='_blank' className='btn'>Download CV</a>
-        <a href="#contact" className='btn btn-primary'>Contact me</a>
-      </div>
-  )
-}
+	return (
+		<div className='cta'>
+			<a
+				href='https://res.cloudinary.com/dhoecmw9w/image/upload/v1668771398/production/Aleksejs_Brehovs_Resume_18-11_u3r5dt.pdf'
+				download
+				rel='noreferrer'
+				target='_blank'
+				className='btn'
+			>
+				Download CV
+			</a>
+			<a
+				href='#contact'
+				className='btn btn-primary'
+			>
+				Contact me
+			</a>
+		</div>
+	);
+};
 
-export default CTA
+export default CTA;
